febc5e71bf78aa21a9c1c466e705d68a36898648
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-06-04 07:58:25 -0700

    link to firebase successful

--- js/appsave.js
+++ js/appsave.js
@@ -1,51 +1,50 @@
 (function () {
 
-	var myFirebaseRef;
-	var viewModel;
+	var myFirebaseRef = new Firebase("https://masumi.firebaseio.com/site");
+	var fbStudents = myFirebaseRef.child('students');
+	var fbMe = myFirebaseRef.child('me');
+	var fbResource = myFirebaseRef.child('resouce');
+	var fbTest = myFirebaseRef.child('test');
+	var viewModel = new ViewModel();
 
+	//do I need init?
 	function init () {
 		console.log('init');
-
-		// connect to Firebase
-		myFirebaseRef = new Firebase("https://masumi.firebaseio.com/site");
-		fbStudents = myFirebaseRef.child('students');
-		fbMe = myFirebaseRef.child('me');
-		fbResource = myFirebaseRef.child('resouce');
-		fbTest = myFirebaseRef.child('test')
-		
-		function viewModel() {
-			var vm = this;
-			vm.students = KnockoutFire.observable(
-				fbStudents, {
-					"$students": {
-						"content": true
-					}
-				}
-			);
-			console.log(vm.students);
-			
-			//UI state
-			//needs to be made back into observables
-			vm.pages = ko.observableArray(["about", "resource", "studentPage"]),
-			vm.modes = ko.observableArray(["admin", "student"]),
-			vm.currentStudent = ko.observable('') ,
-			vm.currentPage = ko.observable('studentPage'),
-			vm.currentMode = ko.observable('student'),
-
-			//functions to change UI state
-			function setPage(clickedPage) {
-				viewModel.currentPage(clickedPage);
-				console.log("clicked a page, now on " + viewModel.currentPage());
-			}
-
-			function selectStudent (clickedStudent) {
-				viewModel.currentStudent(clickedStudent);
-				console.log("you clicked on a student")
-			}			
-		}
 		
 		ko.applyBindings( viewModel );
 
+	}
+	
+	function ViewModel() {
+		var vm = this;
+		vm.students = KnockoutFire.observable(
+			fbStudents, {
+				//$ means that vm.students is now an observable array
+				"$student": {
+					"name": true
+				}
+			}
+		);
+		
+		//UI state
+		vm.pages = ["about", "resource", "studentPage"],
+		vm.modes = ["admin", "student"],
+		vm.currentStudent = ko.observable({
+			name: ko.observable("foo")
+		}) ,
+		vm.currentPage = ko.observable('studentPage'),
+		vm.currentMode = ko.observable('student'),
+
+		//functions to change UI state
+		vm.setPage = function(clickedPage) {
+			vm.currentPage(clickedPage);
+			console.log("clicked a page, now on " + viewModel.currentPage());
+		};
+
+		vm.selectStudent = function() {
+			vm.currentStudent(this);
+			console.log("you clicked on a student", this.name(), "current student is ", vm.currentStudent().name());
+		};			
 	}
 
 	init();
