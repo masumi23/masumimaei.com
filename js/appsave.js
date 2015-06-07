d357b8bb491fb2a7ef0cd6d40e5dba7f3b009171
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-06-07 09:26:08 -0700

    add jscs, enforce jscs rules

--- js/appsave.js
+++ js/appsave.js
@@ -1,94 +1,94 @@
-(function () {
+(function() {
 
-	var myFirebaseRef = new Firebase("https://masumi.firebaseio.com/site");
-	var fbStudents = myFirebaseRef.child('students');
-	var fbMe = myFirebaseRef.child('me');
-	var fbResource = myFirebaseRef.child('resouce');
-	var fbTest = myFirebaseRef.child('test');
-	var viewModel = new ViewModel();
+  var myFirebaseRef = new Firebase('https://masumi.firebaseio.com/site');
+  var fbStudents = myFirebaseRef.child('students');
+  var fbMe = myFirebaseRef.child('me');
+  var fbResource = myFirebaseRef.child('resouce');
+  var fbTest = myFirebaseRef.child('test');
+  var viewModel = new ViewModel();
 
-	//do I need init?
-	function init () {
-		console.log('init');
-		ko.applyBindings( viewModel );
-	}
-	
-	function ViewModel() {
-		var vm = this;
-		vm.students = KnockoutFire.observable(
-			fbStudents, {
-				//$ means that vm.students is now an observable array
-				"$student": {
-					"name": true,
-					"link": true,
-					"repertoire": {
-						"$song": {
-							"name": true
-						}
-					}
-				}
-			}
-		);
-		
-		//UI state
-		vm.pages = [
-		{
-			name: "about",
-			title: "About"
-		}, 
-		{
-			name: "resource",
-			title: "Resources"
-		}, 
-		{
-			name: "studentPage",
-			title: "Your Student Page"
-		}];
-		vm.modes = ["admin", "student"];
-		vm.currentStudent = ko.observable({
-			name: ko.observable(""),
-			link: ko.observable(""),
-			repertoire: ko.observableArray([1,2])
-		});
-		vm.currentPage = ko.observable('studentPage');
-		vm.currentMode = ko.observable('admin');
+  //do I need init?
+  function init() {
+    console.log('init');
+    ko.applyBindings(viewModel);
+  }
 
-		//functions to change UI state
-		vm.setPage = function() {
-			vm.currentPage(this.name);
-		};
+  function ViewModel() {
+    var vm = this;
+    vm.students = KnockoutFire.observable(
+      fbStudents, {
+        //$ means that vm.students is now an observable array
+        '$student': {
+          'name': true,
+          'link': true,
+          'repertoire': {
+            '$song': {
+              'name': true
+            }
+          }
+        }
+      }
+    );
 
-		vm.selectStudent = function() {
-			vm.currentStudent(this);
-			console.log("you clicked on a student", this.name(), "current student is ", vm.currentStudent().name());
-		};	
+    //UI state
+    vm.pages = [
+    {
+      name: 'about',
+      title: 'About'
+    },
+    {
+      name: 'resource',
+      title: 'Resources'
+    },
+    {
+      name: 'studentPage',
+      title: 'Your Student Page'
+    }];
+    vm.modes = ['admin', 'student'];
+    vm.currentStudent = ko.observable({
+      name: ko.observable(''),
+      link: ko.observable(''),
+      repertoire: ko.observableArray([1, 2])
+    });
+    vm.currentPage = ko.observable('studentPage');
+    vm.currentMode = ko.observable('admin');
 
-		vm.addStudent = function() {
-			fbStudents.push({
-				name: "New Student",
-				link: "www.google.com",
-				repertoire: [ {name:'a'}, {name:'b'}]
-			});
-			//how do I console.log this to see the list of rep?
-		};
+    //functions to change UI state
+    vm.setPage = function() {
+      vm.currentPage(this.name);
+    };
 
-		vm.removeStudent = function (item) {
-			fbStudents.child(item.firebase.key()).remove();
-			console.log(item);
-		};	
+    vm.selectStudent = function() {
+      vm.currentStudent(this);
+      console.log('you clicked on a student', this.name(),
+          'current student is ', vm.currentStudent().name());
+    };
 
-		vm.addSong = function(item) {
-			vm.currentStudent().firebase.child('repertoire').push({name:"song"});
-		};
+    vm.addStudent = function() {
+      fbStudents.push({
+        name: 'New Student',
+        link: 'www.google.com',
+        repertoire: [{name: 'a'}, {name: 'b'}]
+      });
+      //how do I console.log this to see the list of rep?
+    };
 
-		vm.removeSong = function(item) {
-			vm.currentStudent().firebase.child('repertoire').child(item.firebase.key()).remove();
-			console.log("removed song");
-			console.log(item);
-		};
-	}
+    vm.removeStudent = function(item) {
+      fbStudents.child(item.firebase.key()).remove();
+      console.log(item);
+    };
 
-	init();
-})()
+    vm.addSong = function(item) {
+      vm.currentStudent().firebase.child('repertoire').push({name: 'song'});
+    };
 
+    vm.removeSong = function(item) {
+      vm.currentStudent().firebase.child('repertoire').child(item.firebase.key()).remove();
+      console.log('removed song');
+      console.log(item);
+    };
+  }
 
+  init();
+
+})();
