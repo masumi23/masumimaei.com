a3bf84e2979bfd9a4ff4abeacd34df6f4c611b06
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-06-08 20:35:22 -0700

    change resources to be contentEditable

--- js/appsave.js
+++ js/appsave.js
@@ -6,7 +6,31 @@
   var fbResource;
   var viewModel;
 
-  //do I need init?
+  // from http://jsfiddle.net/rniemeyer/JksKx/9/
+  ko.bindingHandlers.htmlValue = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+      ko.utils.registerEventHandler(element, 'keyup', function() {
+        var modelValue = valueAccessor();
+        var elementValue = element.innerHTML;
+        if (ko.isWriteableObservable(modelValue)) {
+          modelValue(elementValue);
+
+        } else { //handle non-observable one-way binding
+          var allBindings = allBindingsAccessor();
+          if (allBindings['_ko_property_writers'] && allBindings['_ko_property_writers'].htmlValue) {
+            allBindings['_ko_property_writers'].htmlValue(elementValue);
+          }
+        }
+      });
+    },
+    update: function(element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor()) || '';
+      if (element.innerHTML !== value) {
+        element.innerHTML = value;
+      }
+    }
+  };
+
   function init() {
     console.log('initing');
 
